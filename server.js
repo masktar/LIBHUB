e75d32394d8aa990db13be9d57040059ceac5242
masktar <manhtan0209@gmail.com>
2025-09-16 01:18:31 +0700

    fix: update server config and env variables

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const mysql = require("mysql2");
+const cors = require("cors");
 
 require("dotenv").config();
 
 const app = express();
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
